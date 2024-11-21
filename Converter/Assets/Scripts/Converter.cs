6467f4fa9d8762af708f139cf7eecc909398e707
Denis Kharchevnikov <kharchevnikov@reaction.games>
2024-11-21 10:51:33 +0300

    added converter tdd implementation

--- Converter/Assets/Scripts/Converter.cs
+++ Converter/Assets/Scripts/Converter.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
 namespace Homework
 {
     /**
@@ -22,9 +26,148 @@ namespace Homework
        - Время преобразования ресурсов
        - Состояние: вкл/выкл
      */
-    
     public sealed class Converter
     {
         //TODO: Написать конвертер ресурсов по TDD
+        private readonly int _loadingCapacity;
+        private readonly int _unloadingCapacity;
+        private readonly int _fromResourcesAmount;
+        private readonly int _toResourcesAmount;
+        private readonly float _conversionTime;
+        
+        private float _timer;
+        private bool _isEnabled;
+        
+        public bool Converting { get; private set; }
+        public int LoadingValue { get; private set; }
+        public int UnloadingValue { get; private set; }
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            private set
+            {
+                _isEnabled = value;
+                if (_isEnabled)
+                {
+                    StartConvert();
+                }
+                else
+                {
+                    if (Converting)
+                    {
+                        AddLoadingValue(_fromResourcesAmount);
+                        Converting = false;
+                    }
+                }
+            }
+        }
+
+        private float Timer
+        {
+            get => _timer;
+            set
+            {
+                _timer = Math.Max(0, value);
+                if (_timer <= 0)
+                {
+                    if (Converting)
+                    {
+                        CompleteConvert();
+                    }
+                    else
+                    {
+                        TryStartConverter();   
+                    }
+                }
+            }
+        }
+
+        public event Action OnConverted;
+
+        public Converter(
+            in int loadingCapacity,
+            in int unloadingCapacity,
+            in int fromResourcesAmount,
+            in int toResourcesAmount,
+            in float conversionTime)
+        {
+            if (loadingCapacity <= 0) throw new ArgumentOutOfRangeException($"{nameof(loadingCapacity)} must be greater zero");
+            if (unloadingCapacity <= 0) throw new ArgumentOutOfRangeException($"{nameof(unloadingCapacity)} must be greater zero");
+            if (fromResourcesAmount <= 0) throw new ArgumentOutOfRangeException($"{nameof(fromResourcesAmount)} must be greater zero");
+            if (toResourcesAmount <= 0) throw new ArgumentOutOfRangeException($"{nameof(toResourcesAmount)} must be greater zero");
+            if (conversionTime <= 0.0f) throw new ArgumentOutOfRangeException($"{nameof(conversionTime)} must be greater zero");
+            
+            _loadingCapacity = loadingCapacity;
+            _unloadingCapacity = unloadingCapacity;
+            _fromResourcesAmount = fromResourcesAmount;
+            _toResourcesAmount = toResourcesAmount;
+            _conversionTime = conversionTime;
+
+            LoadingValue = 0;
+            UnloadingValue = 0;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (IsEnabled)
+            {
+                Timer -= deltaTime;
+            }
+        }
+
+        public void Enable() => IsEnabled = true;
+
+        public void Disable() => IsEnabled = false;
+
+        public void AddLoadingValue(in int value)
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException();
+            
+            LoadingValue = Math.Min(_loadingCapacity, LoadingValue + value);
+        }
+        
+        internal void AddUnloadingValue(in int value)
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException();
+
+            UnloadingValue = Math.Min(_unloadingCapacity, UnloadingValue + value);
+        }
+
+        public void RemoveUnloadingValue(in int value)
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException();
+
+            UnloadingValue -= value;
+            TryStartConverter();
+        }
+
+        private void TryStartConverter()
+        {
+            if (!Converting && CanStartConverter())
+            {
+                StartConvert();
+            }
+        }
+
+        internal bool CanStartConverter() =>
+            LoadingValue >= _fromResourcesAmount &&
+            UnloadingValue + _toResourcesAmount <= _unloadingCapacity;
+
+        private void StartConvert()
+        {
+            if (CanStartConverter())
+            {
+                LoadingValue -= _fromResourcesAmount;
+                Timer = _conversionTime;
+                Converting = true;
+            }
+        }
+
+        private void CompleteConvert()
+        {
+            UnloadingValue = Math.Min(_unloadingCapacity, UnloadingValue + _toResourcesAmount);
+            Converting = false;
+            OnConverted?.Invoke();
+        }
     }
 }
--- Converter/Assets/Tests/ConverterTests.cs
+++ Converter/Assets/Tests/ConverterTests.cs
@@ -1,7 +1,253 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
 namespace Homework
 {
     public sealed class ConverterTests
     {
-        //TODO: Написать конвертер ресурсов по TDD
+        private const int DefaultConvertCapacity = 10;
+        private const float DefaultConvertCycleTime = 5.0f;
+        
+        [TestCaseSource(nameof(CorrectConverterCases))]
+        public void WhenInstantiateWithCorrectParamsThenNotNull(
+            int loadingCapacity,
+            int unloadingCapacity,
+            int loadingZoneAmount,
+            int unloadingZoneAmount,
+            float conversionTime)
+        {
+            var converter = new Converter(loadingCapacity, unloadingCapacity, loadingZoneAmount,
+                unloadingZoneAmount, conversionTime);
+            
+            Assert.IsNotNull(converter);
+        }
+
+        private static IEnumerable<TestCaseData> CorrectConverterCases()
+        {
+            yield return new TestCaseData(1, 1, 1, 1, 1.0f).SetName("Case 1");
+            yield return new TestCaseData(1, 2, 3, 4, 5.0f).SetName("Case 2");
+            yield return new TestCaseData(3, 5, 7, 9, 1.0f).SetName("Case 3");
+            yield return new TestCaseData(2, 4, 6, 8, 1.0f).SetName("Case 4");
+            yield return new TestCaseData(9, 8, 7, 6, 0.1f).SetName("Case 5");
+        }
+
+        [TestCaseSource(nameof(IncorrectConverterCases))]
+        public void WhenInstantiateWithIncorrectParamsThenException(
+            int loadingCapacity,
+            int unloadingCapacity,
+            int loadingZoneAmount,
+            int unloadingZoneAmount,
+            float conversionTime)
+        {
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                var converter = new Converter(loadingCapacity, unloadingCapacity, loadingZoneAmount,
+                    unloadingZoneAmount, conversionTime);
+            });
+        }
+
+        private static IEnumerable<TestCaseData> IncorrectConverterCases()
+        {
+            yield return new TestCaseData(1, 0, 1, 1, 1.0f).SetName("Case 1");
+            yield return new TestCaseData(1, 2, 3, -1, 5.0f).SetName("Case 2");
+            yield return new TestCaseData(3, 5, 7, 9, 0.0f).SetName("Case 3");
+            yield return new TestCaseData(-1, 4, 6, 8, -1.0f).SetName("Case 4");
+            yield return new TestCaseData(9, 8, 7, 6, -0.1f).SetName("Case 5");
+        }
+
+        [Test]
+        public void WhenAddExtraLoadingValueThenLimitItByCapacity()
+        {
+            var converter = CreateDefaultConverter();
+
+            converter.AddLoadingValue(DefaultConvertCapacity * 1000);
+            
+            Assert.AreEqual(converter.LoadingValue, DefaultConvertCapacity);
+        }
+        
+        [Test]
+        public void WhenAddExtraUnloadingValueThenLimitItByCapacity()
+        {
+            var converter = CreateDefaultConverter();
+
+            converter.AddUnloadingValue(DefaultConvertCapacity * 1000);
+            
+            Assert.AreEqual(converter.UnloadingValue, DefaultConvertCapacity);
+        }
+        
+        [Test]
+        public void WhenEnableConverterThenIsEnabledTrue()
+        {
+            var converter = CreateDefaultConverter();
+
+            converter.Enable();
+            
+            Assert.IsTrue(converter.IsEnabled);
+        }
+        
+        [Test]
+        public void WhenDisableConverterThenIsEnabledFalse()
+        {
+            var converter = CreateDefaultConverter();
+
+            converter.Disable();
+            
+            Assert.IsFalse(converter.IsEnabled);
+        }
+
+        [Test]
+        public void WhenStartConvertCycleThenLoadingCountDecreasing()
+        {
+            var converter = CreateDefaultConverter();
+            converter.AddLoadingValue(DefaultConvertCapacity);
+            
+            converter.Enable();
+
+            Assert.AreEqual(converter.LoadingValue, DefaultConvertCapacity - 3);
+        }
+        
+        [Test]
+        public void WhenConvertCycleThenUnloadingCountIncreasing()
+        {
+            var converter = CreateDefaultConverter();
+            converter.AddLoadingValue(DefaultConvertCapacity);
+            
+            converter.Enable();
+            converter.Update(DefaultConvertCycleTime);
+
+            Assert.AreEqual(converter.UnloadingValue, 2);
+        }
+        
+        [Test]
+        public void WhenBreakConvertCycleThenLoadingCountReset()
+        {
+            var converter = CreateDefaultConverter();
+            converter.AddLoadingValue(DefaultConvertCapacity);
+            
+            converter.Enable();
+            converter.Update(DefaultConvertCycleTime / 2);
+            converter.Disable();
+
+            Assert.AreEqual(converter.LoadingValue, DefaultConvertCapacity);
+            Assert.AreEqual(converter.UnloadingValue, 0);
+            Assert.AreEqual(converter.Converting, false);
+            Assert.AreEqual(converter.IsEnabled, false);
+        }
+
+        [Test]
+        public void WhenAddNegativeLoadingValueThenException()
+        {
+            var converter = CreateDefaultConverter();
+
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                converter.AddLoadingValue(-1);
+            });
+        }
+        
+        [Test]
+        public void WhenAddNegativeUnloadingValueThenException()
+        {
+            var converter = CreateDefaultConverter();
+
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                converter.AddUnloadingValue(-1);
+            });
+        }
+        
+        [Test]
+        public void WhenRemoveNegativeUnloadingValueThenException()
+        {
+            var converter = CreateDefaultConverter();
+
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                converter.RemoveUnloadingValue(-1);
+            });
+        }
+        
+        [Test]
+        public void WhenNoLoadingSpaceThenNotConverting()
+        {
+            var converter = CreateDefaultConverter();
+
+            converter.Enable();
+            
+            Assert.IsFalse(converter.Converting);
+            Assert.IsFalse(converter.CanStartConverter());
+        }
+        
+        [Test]
+        public void WhenFullUnloadingSpaceThenNotConverting()
+        {
+            var converter = CreateFullConverter();
+
+            converter.Enable();
+            
+            Assert.IsFalse(converter.Converting);
+            Assert.IsFalse(converter.CanStartConverter());
+        }
+        
+        [Test]
+        public void WhenConvertCycleThenAllSpacesChanging()
+        {
+            var converter = CreateDefaultConverter();
+            converter.AddLoadingValue(DefaultConvertCapacity);
+            int loadingValue = 0;
+            int unloadingValue = 0;
+            converter.OnConverted += () =>
+            {
+                loadingValue = converter.LoadingValue;
+                unloadingValue = converter.UnloadingValue;
+            };
+
+            converter.Enable();
+            converter.Update(DefaultConvertCycleTime);
+            
+            Assert.AreEqual(loadingValue, DefaultConvertCapacity - 3);
+            Assert.AreEqual(unloadingValue, 2);
+        }
+        
+        [Test]
+        public void WhenConvertAllAvailableLoadingValueThenPauseConverting()
+        {
+            var converter = CreateDefaultConverter();
+            converter.AddLoadingValue(DefaultConvertCapacity);
+            int convertCount = 0;
+            converter.OnConverted += () => convertCount++;
+
+            converter.Enable();
+            for (int i = 0; i < 5; i++)
+            {
+                converter.Update(DefaultConvertCycleTime);
+            }
+            
+            Assert.AreEqual(convertCount, 3);
+            Assert.AreEqual(converter.LoadingValue, 1);
+            Assert.AreEqual(converter.UnloadingValue, 6);
+            Assert.IsFalse(converter.Converting);
+            Assert.IsTrue(converter.IsEnabled);
+        }
+
+        private Converter CreateDefaultConverter() => 
+            new(DefaultConvertCapacity, 
+                DefaultConvertCapacity, 
+                3, 
+                2, 
+                DefaultConvertCycleTime);
+        
+        private Converter CreateFullConverter()
+        {
+            var converter = new Converter(DefaultConvertCapacity,
+                DefaultConvertCapacity,
+                3,
+                2,
+                DefaultConvertCycleTime);
+            converter.AddLoadingValue(DefaultConvertCapacity);
+            converter.AddUnloadingValue(DefaultConvertCapacity);
+            return converter;
+        }
     }
 }
